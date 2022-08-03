5c3b1b2c377dcfa14c8ec7a0db062b272df98151
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-03 08:38:06 -0700

    working on local storage

--- script/backup.js
+++ script/backup.js
@@ -1,8 +1,17 @@
 var today = new Date();
+
 var hour = today.getHours();
+
+var minutes = today.getMinutes();
+var subMinutes = today.getSeconds();
+var subSeconds = today.getMilliseconds();
+
 
 var el = document.getElementById('currentDay');
 el.innerHTML = today.toDateString();
+
+// var superAccurateTime = document.createElement('h3');
+// superAccurateTime.textContent = `The current time is ${hour}:${minutes}:${subMinutes}`;
 
 var timeBlocks = $('.time-block');
 
@@ -11,9 +20,29 @@ divRow.addClass('row')
 timeBlocks.append(divRow)
 
 var allRows = $('.row')
-var saveBtn = $('.saveBtn')
 
-function scheduler() {
+
+var saveBtn = $('.saveBtn')
+// var textarea = $('.textarea')
+
+
+
+// var readableText = JSON.stringify(textarea)
+
+// function saveSchedule() {
+  
+//   // for (var i = 0; i < textarea.length; i++) {
+//   // localStorage.setItem("Input", textarea[i].val())
+//   // }
+
+
+
+
+
+  
+// }
+
+function renderHours() {
     var workHours = [
       '8',
       '9',
@@ -27,7 +56,8 @@ function scheduler() {
       '17',
     ];
   
-    for (var i = 0; i < workHours.length; i++) { 
+    for (var i = 0; i < workHours.length; i++) {
+      
     var suffix = workHours[i] >= 12 ? "PM" : "AM";
     var hours = ((workHours[i] - 1) % 12 + 1) + " " + suffix 
 
@@ -39,17 +69,15 @@ function scheduler() {
     var textInput = $('<textarea>')
     textInput.addClass('textarea col-xl-10 col-md-10 col-10')
     allRows.append(textInput)
-    
+
+   
+
 
     var saveBtn = $('<button>')
     saveBtn.addClass('saveBtn col-xl-1 col-md-1 col-1')
     allRows.append(saveBtn)  
-    saveBtn.on('click', function(event) {
-      // alert("hi")
-      event.preventDefault();
-      localStorage.setItem("Input", JSON.stringify(textInput.text()));
-    })
-    
+
+    // saveBtn.on('click', saveSchedule)
     
     if (workHours[i] == hour) {
       textInput.addClass("present")
@@ -59,8 +87,25 @@ function scheduler() {
       textInput.addClass("past")
     }
   }
-  JSON.parse(localStorage.getItem("Input"))
+
+  // var textarea = $('.textarea')
+  saveBtn.on('click', $('.textarea').each( function(i){
+
+    localStorage.setItem("Input", $(this[i]).eq().val())
+  })  
+  )
+  localStorage.getItem('Input')
+  
 }
 
 
-scheduler();
+renderHours();
+
+/*
+ saveBtn.on('click', function() {
+      // alert("hi")
+      // var betterObj = JSON.stringify(textInput)
+      localStorage.setItem("Input", textInput)
+    })
+
+*/
